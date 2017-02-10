a9033109f143e630f11ef11984cecd04173da89f
777720 <627078086@qq.com>
2017-02-10 17:29:49 +0800

    react和webpack自己的整合

---
+++ ReactWebpackDemo/app/main.js
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom'
+import React from 'react'
+import Component1 from './components/Component1'
+ReactDom.render(
+  <Component1 />,
+  document.getElementById('content')
+)
